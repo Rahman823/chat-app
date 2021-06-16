fbafbd8014847d10ef16bf6e7041cf592f40aa99
rahman <loveyougit@.com>
2021-06-16 16:42:02 +0530

    initialize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+
+const config = {
+  apiKey: "AIzaSyD3IUx0Kh5MD3152YbBaelLU6r-uGTQ69g",
+  authDomain: "chat-web-app-f7198.firebaseapp.com",
+  projectId: "chat-web-app-f7198",
+  storageBucket: "chat-web-app-f7198.appspot.com",
+  messagingSenderId: "392974566869",
+  appId: "1:392974566869:web:6143636ec5e98e81f1e95c"
+};
+
+const app = firebase.initializeApp(config);
